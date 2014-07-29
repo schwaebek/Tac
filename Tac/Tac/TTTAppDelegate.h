84e68edcad9e8d88ba7cb223f508139bd1057bc4
schwaebek <schwaebek@Katlyns-MacBook-Pro.local>
2014-07-29 09:36:36 -0400

    Create App

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by Katlyn Schwaebe on 7/29/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
